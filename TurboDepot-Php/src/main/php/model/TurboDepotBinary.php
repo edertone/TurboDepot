e437bcba1a4edf73e028fd959887d9d9fea512ba
Jaume Mussons <jaume.mussons@edertone.com>
2018-06-29 19:19:55 +0200

    added raw code

---
+++ TurboDepot-Php/src/main/php/model/TurboDepotBinary.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * TurboDepot is a cross language ORM library that allows saving, retrieving, listing, filtering and more with complex class data instances
+ *
+ * Website : -> http://www.turbodepot.org
+ * License : -> Licensed under the Apache License, Version 2.0. You may not use this file except in compliance with the License.
+ * License Url : -> http://www.apache.org/licenses/LICENSE-2.0
+ * CopyRight : -> Copyright 2017 Edertone Advanded Solutions (Barcelona). http://www.edertone.com
+ */
+
+namespace org\turbodepot\src\main\php\model;
+
+
+/**
+ * TODO
+ */
+abstract class TurboDepotBinary {
+
+
+
+}
+
+?>
