e11ac1b9cf23fb51b40bb2606e43e9dc90cd277c
Jaume Mussons <jaume.mussons@edertone.com>
2019-02-06 17:10:06 +0100

    Added copyright headers and namespace validations

--- TurboDepot-Php/src/main/php/managers/DepotManager.php
+++ TurboDepot-Php/src/main/php/managers/DepotManager.php
@@ -1,16 +1,16 @@
 <?php
 
 /**
- * TurboUsers is a cross language fully featured user management library
+ * TurboDepot is a cross language super charged ORM library to store and read multiple types of data objects
  *
- * Website : -> http://www.turboframework.org
+ * Website : -> http://www.turbodepot.org
  * License : -> Licensed under the Apache License, Version 2.0. You may not use this file except in compliance with the License.
  * License Url : -> http://www.apache.org/licenses/LICENSE-2.0
  * CopyRight : -> Copyright 2019 Edertone Advanded Solutions. http://www.edertone.com
  */
 
 
-namespace org\turbousers\src\main\php\managers;
+namespace org\turbodepot\src\main\php\managers;
 
 use org\turbocommons\src\main\php\model\BaseStrictClass;
 
