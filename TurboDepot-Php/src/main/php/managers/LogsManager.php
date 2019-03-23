4d1d7303b32356a28306c7a9b22c79fc7b33afd7
Jaume Mussons <jaume.mussons@edertone.com>
2019-03-23 17:57:07 +0100

    Implemented LogsManager write method and tests

--- TurboDepot-Php/src/main/php/managers/LogsManager.php
+++ TurboDepot-Php/src/main/php/managers/LogsManager.php
@@ -12,15 +12,126 @@
 
 namespace org\turbodepot\src\main\php\managers;
 
+use DateTime;
+use UnexpectedValueException;
 use org\turbocommons\src\main\php\model\BaseStrictClass;
+use org\turbocommons\src\main\php\utils\StringUtils;
 
 
 /**
- * Manages all the logs that are stored on the depot
+ * logs manager class
  */
 class LogsManager extends BaseStrictClass{
 
 
+    /**
+     * Defines how many blank lines will be added after each write() call is performed
+     *
+     * @var integer
+     */
+    public $blankLinesBetweenLogs = 0;
+
+
+    /**
+     * The full filesystem path to the root of the folder where logs are stored
+     * @var string
+     */
+    private $_rootPath = '';
+
+
+    /**
+     * Centralized logs manager.
+     * It can be used standalone when a full depot is not necessary.
+     * If we want to use it as part of a depot instance, we must interact with our logs via the DepotManager class.
+     *
+     * @param string $rootPath The full absolute filesystem path to the root of the folder where logs will be output
+     */
+    public function __construct(string $rootPath){
+
+        if(!is_dir($rootPath)){
+
+            throw new UnexpectedValueException('LogsManager received an invalid rootPath: '.$rootPath);
+        }
+
+        $this->_rootPath = $rootPath;
+    }
+
+
+    /**
+     * Add text to the specified log file
+     *
+     * @param string $text The text to be added to the log file
+     * @param string $logFile A log fine name or a relative path (based on the currently loaded logs root path) where the text will be written
+     * @param bool $writeDateAndTime If set to true, each log entry that is saved with write() method will start with the current date and time
+     * @param bool $createFile If set to true and the target log file does not exist, it will be created. If set to false and it does not exist, an exception will be thrown
+     *
+     * @return void
+     */
+    public function write(string $text, string $logFile, bool $writeDateAndTime = true, bool $createFile = true){
+
+        $filePath = StringUtils::formatPath($this->_rootPath.DIRECTORY_SEPARATOR.$logFile, DIRECTORY_SEPARATOR);
+
+        if(StringUtils::isEmpty($logFile)){
+
+            throw new UnexpectedValueException('logFile must not be empty');
+        }
+
+        if(!is_file($filePath) && !$createFile){
+
+            throw new UnexpectedValueException('Log file does not exist and createFile is false: '.$logFile);
+        }
+
+        // Create the subfolder if necessary
+        if(!is_dir(StringUtils::getPath($filePath))){
+
+            (new FilesManager())->createDirectory(StringUtils::getPath($filePath), true);
+        }
+
+        // Open a pointer to the end of the file to append the new text
+        // If the file does not exist, it will be created
+        $filePointer = fopen($filePath, 'a+');
+
+        if($filePointer === false){
+
+            throw new UnexpectedValueException('Could not access the log file: '.$logFile);
+        }
+
+        // Acquire exclusive lock. If any other process is already writting to this file and has it locked,
+        // this method will wait till the lock is released
+        if(flock($filePointer, LOCK_EX)) {
+
+            $blankLines = "\n";
+
+            for ($i = 0; $i < $this->blankLinesBetweenLogs; $i++) {
+
+                $blankLines .= "\n";
+            }
+
+            $date = $writeDateAndTime ? DateTime::createFromFormat('U.u', microtime(true))->format("Y-m-d H:i:s.u").' ' : '';
+
+            fwrite($filePointer, $date.$text.$blankLines);
+
+            // flush output before releasing the lock
+            fflush($filePointer);
+
+            // Release the lock so other processes can write to the file
+            flock($filePointer, LOCK_UN);
+
+        } else {
+
+            throw new UnexpectedValueException('Could not lock the log file: '.$logFile);
+        }
+
+        fclose($filePointer);
+    }
+
+
+    /**
+     * TODO
+     */
+    public function truncate(string $logFile){
+
+    }
 }
 
 ?>
---
+++ TurboDepot-Php/src/test/php/managers/LogsManagerTest.php
@@ -0,0 +1,244 @@
+<?php
+
+/**
+ * TurboDepot is a cross language ORM: Save, read, list, filter and easily perform any storage operation with your application objects
+ *
+ * Website : -> http://www.turbodepot.org
+ * License : -> Licensed under the Apache License, Version 2.0. You may not use this file except in compliance with the License.
+ * License Url : -> http://www.apache.org/licenses/LICENSE-2.0
+ * CopyRight : -> Copyright 2019 Edertone Advanded Solutions (08211 Castellar del Vallès, Barcelona). http://www.edertone.com
+ */
+
+namespace org\turbodepot\src\test\php\managers;
+
+use PHPUnit\Framework\TestCase;
+use DateTime;
+use stdClass;
+use Throwable;
+use org\turbocommons\src\main\php\managers\ValidationManager;
+use org\turbocommons\src\main\php\utils\ArrayUtils;
+use org\turbocommons\src\main\php\utils\NumericUtils;
+use org\turbocommons\src\main\php\utils\StringUtils;
+use org\turbodepot\src\main\php\managers\LogsManager;
+use org\turbodepot\src\main\php\managers\FilesManager;
+
+
+/**
+ * LogsManagerTest tests
+ *
+ * @return void
+ */
+class LogsManagerTest extends TestCase {
+
+
+    /**
+     * @see TestCase::setUpBeforeClass()
+     *
+     * @return void
+     */
+    public static function setUpBeforeClass(){
+
+        require_once __DIR__.'/../resources/libs/turbocommons-php-1.0.0.phar';
+    }
+
+
+    /**
+     * @see TestCase::setUp()
+     *
+     * @return void
+     */
+    protected function setUp(){
+
+        $this->exceptionMessage = '';
+
+        $this->filesManager = new FilesManager();
+
+        // Create a temporary folder
+        $this->tempFolder = $this->filesManager->createTempDirectory('TurboCommons-LogsManagerTest');
+        $this->assertTrue(strpos($this->tempFolder, 'TurboCommons-LogsManagerTest') !== false);
+        $this->assertTrue($this->filesManager->isDirectoryEmpty($this->tempFolder));
+        $this->assertFalse($this->filesManager->isFile($this->tempFolder));
+
+        $this->sut = new LogsManager($this->tempFolder);
+    }
+
+
+    /**
+     * @see TestCase::tearDown()
+     *
+     * @return void
+     */
+    protected function tearDown(){
+
+        // Delete temporary folder
+        $this->filesManager->deleteDirectory($this->tempFolder);
+
+        if($this->exceptionMessage != ''){
+
+            $this->fail($this->exceptionMessage);
+        }
+    }
+
+
+    /**
+     * @see TestCase::tearDownAfterClass()
+     *
+     * @return void
+     */
+    public static function tearDownAfterClass(){
+
+        // Nothing necessary here
+    }
+
+
+    /**
+     * testConstruct
+     *
+     * @return void
+     */
+    public function testConstruct(){
+
+        // Test empty values
+        try {
+            $this->sut = new LogsManager(null);
+            $this->exceptionMessage = 'null did not cause exception';
+        } catch (Throwable $e) {
+            $this->assertRegExp('/must be of the type string, null given/', $e->getMessage());
+        }
+
+        try {
+            $this->sut = new LogsManager(0);
+            $this->exceptionMessage = 'null did not cause exception';
+        } catch (Throwable $e) {
+            $this->assertRegExp('/LogsManager received an invalid rootPath: 0/', $e->getMessage());
+        }
+
+        try {
+            $this->sut = new LogsManager('');
+            $this->exceptionMessage = 'null did not cause exception';
+        } catch (Throwable $e) {
+            $this->assertRegExp('/LogsManager received an invalid rootPath: /', $e->getMessage());
+        }
+
+        try {
+            $this->sut = new LogsManager('          ');
+            $this->exceptionMessage = 'null did not cause exception';
+        } catch (Throwable $e) {
+            $this->assertRegExp('/LogsManager received an invalid rootPath:           /', $e->getMessage());
+        }
+
+        // Test ok values
+        $this->assertSame('org\turbodepot\src\main\php\managers\LogsManager', get_class(new LogsManager($this->tempFolder)));
+
+        // Test wrong values
+        try {
+            $this->sut = new LogsManager('invalid/path/here');
+            $this->exceptionMessage = 'null did not cause exception';
+        } catch (Throwable $e) {
+            $this->assertRegExp('/LogsManager received an invalid rootPath: invalid\/path\/here/', $e->getMessage());
+        }
+
+        try {
+            $this->sut = new LogsManager([1, 2, 3]);
+            $this->exceptionMessage = '[1, 2, 3] did not cause exception';
+        } catch (Throwable $e) {
+            $this->assertRegExp('/must be of the type string, array given/', $e->getMessage());
+        }
+
+        // Test exceptions
+        // Already tested;
+    }
+
+
+    /**
+     * testWrite
+     *
+     * @return void
+     */
+    public function testWrite(){
+
+        $sep = $this->filesManager->dirSep();
+
+        // Test empty values
+        try {
+            $this->sut->write(null, null);
+            $this->exceptionMessage = 'null did not cause exception';
+        } catch (Throwable $e) {
+            $this->assertRegExp('/must be of the type string, null given/', $e->getMessage());
+        }
+
+        try {
+            $this->sut->write('', '');
+            $this->exceptionMessage = '"" did not cause exception';
+        } catch (Throwable $e) {
+            $this->assertRegExp('/logFile must not be empty/', $e->getMessage());
+        }
+
+        // Test ok values
+        $this->sut->write('some text', 'testFile');
+        $this->assertRegExp('/....-..-.. ..:..:......... some text\n/', $this->filesManager->readFile($this->tempFolder.$sep.'testFile'));
+
+        $this->sut->write('some text', 'testFile');
+        $this->assertRegExp('/....-..-.. ..:..:......... some text\n....-..-.. ..:..:......... some text\n/',
+            $this->filesManager->readFile($this->tempFolder.$sep.'testFile'));
+
+        $this->sut->write('another text', 'testFile');
+        $this->assertRegExp('/....-..-.. ..:..:......... some text\n....-..-.. ..:..:......... some text\n....-..-.. ..:..:......... another text\n/',
+            $this->filesManager->readFile($this->tempFolder.$sep.'testFile'));
+
+        $this->sut->write('some text', 'fileWithoutDate', false);
+        $this->assertRegExp('/some text\n/', $this->filesManager->readFile($this->tempFolder.$sep.'fileWithoutDate'));
+
+        $this->sut->write('a', 'fileWithoutDate', false);
+        $this->sut->write('b', 'fileWithoutDate', false);
+        $this->sut->write('c', 'fileWithoutDate', false);
+        $this->sut->write('d', 'fileWithoutDate', false);
+        $this->sut->write('e', 'fileWithoutDate', false);
+        $this->sut->write('f', 'fileWithoutDate', false);
+        $this->assertRegExp('/some text\na\nb\nc\nd\ne\nf/', $this->filesManager->readFile($this->tempFolder.$sep.'fileWithoutDate'));
+
+        try {
+            $this->sut->write('must not be written', 'mustnotbewritten', false, false);
+            $this->exceptionMessage = 'mustnotbewritten did not cause exception';
+        } catch (Throwable $e) {
+            $this->assertRegExp('/Log file does not exist and createFile is false: mustnotbewritten/', $e->getMessage());
+        }
+
+        $this->assertFalse($this->filesManager->isFile($this->tempFolder.$sep.'mustnotbewritten'));
+
+        $this->sut->write('some text on a subfolder log file', 'subfolder/testFile', false);
+        $this->assertRegExp('/some text on a subfolder log file\n/', $this->filesManager->readFile($this->tempFolder.$sep.'subfolder/testFile'));
+        $this->assertTrue($this->filesManager->isFile($this->tempFolder.$sep.'subfolder'.$sep.'testFile'));
+
+        // Test wrong values
+        // already tested
+
+        // Test exceptions
+        // already tested
+    }
+
+
+    /**
+     * testTruncate
+     *
+     * @return void
+     */
+    public function testTruncate(){
+
+        // Test empty values
+        // TODO
+
+        // Test ok values
+        // TODO
+
+        // Test wrong values
+        // TODO
+
+        // Test exceptions
+        // TODO
+
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+}
+
+?>
